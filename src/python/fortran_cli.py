0c9388e405d93c6e5b682f7a2294d1777ed7b182
40291992 <mmcateer18@qub.ac.uk>
2023-10-20 21:39:19 +0000

    Add CLI and initial file reading logic

---
+++ src/python/fortran_cli.py
@@ -0,0 +1,40 @@
+"""
+The (currently) main entry point for the Fortran 90 code analyser.
+
+Below is a list of the current commands available, as well as their arguments:
+
+print-to-console: Prints the raw contents of a specified file.
+    file-path: The full path to the file you wish to print to the console.
+
+"""
+import click
+
+from fortran_parser import parse_file
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "--file-path",
+    required=True,
+    prompt=True,
+    help="The full path to the file you wish to print to the console.",
+)
+def print_to_console(file_path):
+    try:
+        for line in parse_file(file_path):
+            click.echo(line, nl=False)
+    except FileNotFoundError:
+        click.echo(
+            "Unable to print file contents - the file path you have provided is not valid."
+        )
+    except Exception:
+        click.echo("An unknown error occurred when attempting to read the file.")
+
+
+if __name__ == "__main__":
+    cli()
